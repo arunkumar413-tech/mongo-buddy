651efa00cf10df3969ba7e3457612bcde89b9b3d
arunkumar413-tech <arun.kumar@portl.co>
2026-01-21 00:23:10 +0530

    feat: configure CORS to allow requests from http://localhost:8080

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:8080'
+}));
 app.use(express.json());
 
 let client = null;
